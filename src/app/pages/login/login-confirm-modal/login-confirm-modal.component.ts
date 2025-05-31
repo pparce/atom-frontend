b31bfa7ea432eb61c82631dc0fc03f653eeda02d
Peter <56896988+pparce@users.noreply.github.com>
2025-05-31 11:15:24 +0200

    configurando modulo login

---
+++ src/app/pages/login/login-confirm-modal/login-confirm-modal.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+    selector: 'app-login-confirm-modal',
+    imports: [],
+    templateUrl: './login-confirm-modal.component.html',
+    styleUrl: './login-confirm-modal.component.css',
+})
+export class LoginConfirmModalComponent {
+    email: string = '';
+
+    activeModal = inject(NgbActiveModal);
+
+    onConfirm(){
+        this.activeModal.close(true);
+    }
+
+    onCancel(){
+        this.activeModal.dismiss();
+    }
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,64 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from '@services/auth.service';
+import { ConnectionService } from '@services/connection.service';
+import { ApiRoutes } from 'src/app/api.routes';
+import { LoginConfirmModalComponent } from 'src/app/pages/login/login-confirm-modal/login-confirm-modal.component';
 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  template: `<p>login works!</p>`,
-  styleUrl: './login.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-login',
+    imports: [
+        ReactiveFormsModule,
+    ],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css',
 })
-export class LoginComponent { }
+export class LoginComponent {
+    email = new FormControl('', [
+        Validators.required,
+        Validators.email,
+    ]);
+
+    connectionService = inject(ConnectionService);
+    modalService = inject(NgbModal);
+    router = inject(Router);
+    authService = inject(AuthService);
+
+    onLogin() {
+        this.connectionService.post({ url: ApiRoutes.AUTH_LOGIN, data: this.buildJSON() }).subscribe({
+            next: (response) => {
+                this.authService.setLogin(response);
+                this.router.navigateByUrl('/admin');
+            },
+            error: (error) => {
+                console.error('Login failed', error);
+            }
+        });
+    }
+
+    onConfirmEmail() {
+        let modalRef = this.modalService.open(LoginConfirmModalComponent)
+        modalRef.componentInstance.email = this.email.value;
+        modalRef.result.then((result) => {
+            if (result) {
+                this.createUser();
+            }
+        }
+        ).catch((error) => {
+            console.error('Modal dismissed', error);
+        }
+        );
+    }
+
+    createUser() {
+
+    }
+
+    buildJSON() {
+        return {
+            email: this.email.value
+        }
+    }
+}
